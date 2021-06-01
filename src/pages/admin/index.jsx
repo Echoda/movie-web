a33e8e22b53fa87dafb3865aaa0dc97edff6418c
Echoda <2480954635@qq.com>
2021-06-01 22:54:54 +0800

    admin 框架

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link, Switch, Route } from 'react-router-dom';
+import MovieList from './components/MovieList/index';
+import UserList from './components/UserList/index';
+import './index.less';
 
 export default function Admin() {
     return (
-        <div>
-            <h1>admin 受保护的页面</h1>
+        <div className="admin-page">
+            <div className="header">
+                <p className="title">电影点评网站管理系统</p>
+                <Link to="/home" className="tohome">回到首页</Link>
+            </div>
+            <div className="middle">
+                <ul className="menu">
+                    <li>
+                        <Link to="/admin/movielist" style={{ color: '#000' }}>影片列表</Link>
+                    </li>
+                    <li>
+                        <Link to="/admin/userlist" style={{ color: '#000' }}>用户列表</Link>
+                    </li>
+                </ul>
+                <div className="main">
+                    <Switch>
+                        <Route path="/admin/movielist" component={MovieList} />
+                        <Route path="/admin/userlist" component={UserList} />
+                    </Switch>
+                </div>
+            </div>
         </div>
     )
 }
